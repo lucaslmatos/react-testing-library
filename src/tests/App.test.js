a6f09172a6d2f530e575fdbbd7fda0db4aaa45c6
Lucas Matos <lucaslmatos18@gmail.com>
2023-02-28 15:45:40 -0300

    Implementado testes do componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste 01 : Componente App.js', () => {
+  test('Testa se o primeiro link possui o texto Home', () => {
+    renderWithRouter(<App />);
+    const allLinks = screen.getAllByRole('link');
+    expect(allLinks[0]).toHaveTextContent('Home');
+  });
+  test('Testa se o primeiro link redireciona a página para a URL "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    const allLinks = screen.getAllByRole('link');
+    userEvent.click(allLinks[0]);
+    expect(history.location.pathname).toBe('/');
+  });
+  test('Testa se o segundo link possui o texto About', () => {
+    renderWithRouter(<App />);
+    const allLinks = screen.getAllByRole('link');
+    expect(allLinks[1]).toHaveTextContent('About');
+  });
+  test('Testa se o segundo link redireciona a página para a URL "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+    const allLinks = screen.getAllByRole('link');
+    userEvent.click(allLinks[1]);
+    expect(history.location.pathname).toBe('/about');
+  });
+  test('Testa se o terceiro link possui o texto favorites', () => {
+    renderWithRouter(<App />);
+    const allLinks = screen.getAllByRole('link');
+    expect(allLinks[2]).toHaveTextContent('Favorite Pokémon');
+  });
+  test('Testa se o terceiro link redireciona a página para a URL "/favorites"', () => {
+    const { history } = renderWithRouter(<App />);
+    const allLinks = screen.getAllByRole('link');
+    userEvent.click(allLinks[2]);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+  test('Testa se o terceiro link redireciona a página para a URL "/favorites"', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/xablau');
+    });
+    expect(screen.getByText('Page requested not found')).toBeInTheDocument();
+  });
+});
